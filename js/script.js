5b474acc84dc7096d0e3f5a194a46a87eb0956e5
MathASMotta <rm550352@fiap.com.br>
2023-04-25 20:48:04 -0300

    Aula dia 25/04

--- js/script.js
+++ js/script.js
@@ -11,11 +11,19 @@
 // console.log(arrayElements);
 
 //convertendo node list em array com spread
-const arrayElements = [...document.querySelectorAll("li")];
+let tmp = 0;
 
-arrayElements.map((el,key)=>{
 
-    if (el.textContent.indexOf("1")) {
-        el.textContent = `[ ${el.textContent} ]`;
-    }
-});
+function mudaCor() {
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    
+    const elemento = [...document.getElementsByClassName("conteudo")];
+    elemento.forEach( (el)=>{
+        el.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } )
+
+   tmp = setTimeout(mudaCor, 1000);
+
+}
